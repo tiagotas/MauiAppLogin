73b71f0b439868c52191c07049e6cb152824cc62
tiagotas <tiago@tiago.blog.br>
2024-04-24 12:42:12 -0300

    Finalização do Projeto Login

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -6,7 +6,21 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            string? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+
+                if(usuario_logado == null)
+                {
+                    MainPage = new Login();
+
+                } else
+                {
+                    MainPage = new Protegida();
+                }
+            });            
         }
 
         protected override Window CreateWindow(IActivationState activationState)
